b487df0687b50aa214df918c72a8cf86bbbd433b
DUCVTT <vodp@systena.co.jp>
2017-06-20 13:01:59 +0900

    Add getTestFloat

---
+++ TestProject/TestObject.h
@@ -0,0 +1,13 @@
+//
+//  TestObject.h
+//  TestProject
+//
+//  Created by DUCVTT on 2017/06/20.
+//  Copyright © 2017年 Nifty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
++(float) getTestFloat;
+@end
---
+++ TestProject/TestObject.m
@@ -0,0 +1,15 @@
+//
+//  TestObject.m
+//  TestProject
+//
+//  Created by DUCVTT on 2017/06/20.
+//  Copyright © 2017年 Nifty. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
++(float)getTestFloat{
+    return 0.1;
+}
+@end
