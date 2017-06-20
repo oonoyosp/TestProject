795800a24e351d9a2590fad90cbac5a595543af3
oonoyosp <oonoyosp@systena.co.jp>
2017-06-20 12:50:58 +0900

    first

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by oono on 2017/06/20.
+//  Copyright © 2017年 Nifty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by oono on 2017/06/20.
+//  Copyright © 2017年 Nifty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
